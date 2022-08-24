3691d1a82a47410c0e877141ec677d368463bb9a
gavinbaoliu <gavinbaoliu@tencent.com>
2022-08-24 23:10:30 +0800

    设置 Android 为全屏模式

--- androidApp/src/main/java/net/novate/calculator/android/MainActivity.kt
+++ androidApp/src/main/java/net/novate/calculator/android/MainActivity.kt
@@ -1,16 +1,23 @@
 package net.novate.calculator.android
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import net.novate.calculator.sharedui.CalculatorUI
 
-
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.isDecorFitsSystemWindows = false
         setContent {
             CalculatorUI()
         }
     }
 }
+
+var Window.isDecorFitsSystemWindows: Boolean
+    set(value) = WindowCompat.setDecorFitsSystemWindows(this, value)
+    get() = TODO()
