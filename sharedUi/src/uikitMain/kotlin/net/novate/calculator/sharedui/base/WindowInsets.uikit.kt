8de2a1c1397e7a4600b4ea3dc6bae3d552c3b415
gavinbaoliu <gavinbaoliu@tencent.com>
2022-08-27 23:34:13 +0800

    调整 iOS WindowInsetsProvider 实现

--- sharedUi/src/uikitMain/kotlin/net/novate/calculator/sharedui/base/WindowInsets.uikit.kt
+++ sharedUi/src/uikitMain/kotlin/net/novate/calculator/sharedui/base/WindowInsets.uikit.kt
@@ -6,11 +6,11 @@ import androidx.compose.ui.geometry.Rect
 
 @Composable
 actual fun WindowInsetsProvider(content: @Composable () -> Unit) {
-    // TODO: iOS 待实现
+    // TODO: iOS 待实现，先按照 iPhone 13 写死
     CompositionLocalProvider(
-        LocalStatusBarInsets provides Rect.Zero,
-        LocalNavigationBarInsets provides Rect.Zero,
-        LocalWindowInsets provides Rect.Zero,
+        LocalStatusBarInsets provides Rect(0f, 44f, 0f, 0f),
+        LocalNavigationBarInsets provides Rect(0f, 0f, 0f, 22f),
+        LocalWindowInsets provides Rect(0f, 44f, 0f, 22f),
         content = content
     )
 }
